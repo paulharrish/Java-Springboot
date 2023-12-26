c1e9051f6a2a1226a94080891821de2749d2b9d0
paulharrish <prasannaharrish@gmail.com>
2023-12-26 18:44:00 +0530

    Updated.

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,10 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,18 +15,15 @@ public class HelloController {
         return "Greetings from Spring Boot!";
     }
 
+    @RestController
+    public class ExampleController {
+
+    @PostMapping("/create")
+    public String create(@RequestBody(required = true) String data) {
+    // Process the data and create a resource
+    return "Resource created successfully";
+    }
+    }
+
+
 }
-
-// import org.springframework.stereotype.Controller;
-// import org.springframework.ui.Model;
-// import org.springframework.web.bind.annotation.GetMapping;
-
-// @RestController
-// public class HelloController {
-
-// @GetMapping("/hello")
-// public String hello(Model model) {
-// model.addAttribute("message", "Hello, World!");
-// return "helloPage";
-// }
-// }
---
+++ demo/src/main/java/com/example/demo/Userdata.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+public class Userdata {
+    private String username;
+    private String email;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setter methods
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
